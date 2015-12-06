92c7db13efb7d5213bca256bf34cd69dd8545207
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-06 08:14:49 -0800

    remove CPOSIXRegex dependency

--- Package.swift
+++ Package.swift
@@ -1,8 +1,5 @@
 import PackageDescription
 
 let package = Package(
-	name: "POSIXRegex",
-	dependencies: [
-        .Package(url: "https://github.com/Zewo/CPOSIXRegex.git", majorVersion: 0, minor: 1)
-	]
+	name: "POSIXRegex"
 )
