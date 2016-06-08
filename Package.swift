2c4731b2412f9ee17273c64f556186cb1446b3f7
Dan Appel <dan.appel00@gmail.com>
2016-06-08 00:10:58 -0700

    Update to 05-09 and bump dependencies

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "POSIXRegex",
     dependencies: [
-        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 8),
     ]
 )
